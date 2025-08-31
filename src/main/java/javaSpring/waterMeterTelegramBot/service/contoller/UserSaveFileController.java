e3f16adee362917d89c8caeec98ffaa22d259cf3
Elbaf <georgiyelbaf@gmail.com>
2025-09-01 00:29:09 +0300

    Added @Qualifier annotations for interfaces accepted by the UserSaveFileController class

--- src/main/java/javaSpring/waterMeterTelegramBot/service/contoller/UserSaveFileController.java
+++ src/main/java/javaSpring/waterMeterTelegramBot/service/contoller/UserSaveFileController.java
@@ -1,29 +1,35 @@
 package javaSpring.waterMeterTelegramBot.service.contoller;
 
 import javaSpring.waterMeterTelegramBot.data.user.User;
-import javaSpring.waterMeterTelegramBot.repository.user.SaveFileUser;
+import javaSpring.waterMeterTelegramBot.repository.user.SaverUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserSaveFileController implements UserController {
     private final UserController userController;
-    private final SaveFileUser saveFileUser;
-    public UserSaveFileController(UserChangeController userController, SaveFileUser saveFileUser) {
+    private final SaverUser saverUser;
+
+    @Autowired
+    public UserSaveFileController(
+            @Qualifier("userChangeController") UserController userController,
+            @Qualifier("saveFileUser") SaverUser saverUser){
         this.userController = userController;
-        this.saveFileUser = saveFileUser;
+        this.saverUser = saverUser;
     }
 
 
     @Override
     public void drunkWater(User user, int countWaterMl) {
        userController.drunkWater(user, countWaterMl);
-       saveFileUser.save(user);
+       saverUser.save(user);
     }
 
     @Override
     public int countDrunkForDay(User user) {
         int countWaterDrunkML = userController.countDrunkForDay(user);
-        saveFileUser.save(user);
+        saverUser.save(user);
         return countWaterDrunkML;
     }
 }
